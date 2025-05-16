9ea8d279504bfbb50242e9b21939c483d7057942
sapirca <sapir.caduri@gmail.com>
2025-05-16 17:40:10 +0700

    .

--- animation/frameworks/kivsee/kivsee_sequence.py
+++ animation/frameworks/kivsee/kivsee_sequence.py
@@ -7,6 +7,7 @@ from constants import ANIMATION_OUT_TEMP_DIR, KIVSEE_ANIMATION_SUFFIX, KIVSEE_TE
 class KivseeSequence(Sequence):
 
     def __init__(self, sequence_path=None):
+        super().__init__()
         self.sequence_path = sequence_path
         self.sequence_data = None
         self.steps = []
